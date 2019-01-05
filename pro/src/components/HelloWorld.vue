4cb354ac509c1f8b8239b8f9a0b12602bed38253
LXB <biaobin@qq.com>
2019-01-05 14:48:56 +0800

    123

---
+++ pro/src/components/HelloWorld.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="hello">
+ <header>
+ 	<img class="head" :src="src"></img>
+ 	<router-link to="/follow" tag="span">关注</router-link>
+	<router-link to="/discover" tag='span'>发现</router-link>
+	<router-link to="/about" tag='span'>附近</router-link>
+ </header>
+		<section>
+   		<router-view @toparent="getdata"></router-view>
+   	</section>
+   	<footer>
+   		<router-link to="/home" tag="span">首页</router-link>
+   		<router-link to="/mall" tag='span'>商城</router-link>
+   		<router-link to="/photograph" tag='span'>+</router-link>
+   		<router-link to="/news" tag='span'>消息</router-link>
+   		<router-link to='/myself' tag="span">我</router-link>
+   	</footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      src: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.hello{
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+}
+header{
+	position: relative;
+	display: flex;
+	justify-content: center;
+	height: 50px;
+	background: #f1f1f1;
+	text-align: center;
+	line-height: 50px;
+	color: #a1a1a1;
+	font-size: 14px;
+}
+header img{
+	position: absolute;
+	left: 10px;
+	top: 0;
+	width: 50px;
+	height: 50px;
+	overflow: hidden;
+	border-radius: 50%;
+	background: #fff;
+}
+header span{
+	display: block;
+	width: 50px;
+	height: 50px;
+	font-weight: 900;
+}
+header span:hover,header span:first-of-type{
+	color: black;
+}
+section{
+	flex: 1;
+	margin-top: 40px;
+	overflow: auto;
+}
+footer{
+	display: flex;
+	justify-content: space-between;
+	height: 50px;
+	background: #f1f1f1;
+	text-align: center;
+	line-height: 50px;
+	color: #a1a1a1;
+	font-size: 18px;
+}
+footer span{
+	display: block;
+	width: 50px;
+	height: 50px;
+	font-weight: 900;
+}
+footer span:hover{
+	color: black;
+}
+footer span:nth-child(3){
+	border-radius: 50%;
+	font-size: 40px;
+	font-weight: 900;
+	color: black;
+}
+</style>
