910f348b379f32c7825fc608011c3c3028d123d4
LXB <biaobin@qq.com>
2019-01-07 21:33:59 +0800

    xiugai21

--- pro/src/components/About.vue
+++ pro/src/components/About.vue
@@ -1,31 +1,35 @@
 <template>
 	<div id="">
-		<div class="mint-searchbar" style="background: #fff;padding: 8px 40px;">
-			<router-link to="/search" class="mint-searchbar-inner" style="background: #F1F1F1;">
-				<i class="mintui mintui-search"></i>
-				<input type="search" placeholder="大家都在搜“玩转春节”" class="mint-searchbar-core" style="background: #F1F1F1;">
-			</router-link>
-		</div>
 		<div class="benti">
-			<figure>
-				<router-link to='/#'><img src="#" /></router-link>
-				<p>数据模仿</p>
-				<div class="lietu">
-					<img src="*"/>
-					<span>pink</span>
-					<router-link to='#' tag="h5">点赞</router-link>
-				</div>
-			</figure>
-			<figure>
-				<router-link to='/#'><img src="#" /></router-link>
-				<p>数据模仿</p>
-				<div class="lietu">
-					<img src="*"/>
-					<span>pink</span>
-					<router-link to='#' tag="h5">点赞</router-link>
-				</div>
-			</figure>
-			
+			<ul class="discoverList" style="padding: 0 25px;">
+				<li>
+					<router-link to='/#'><img src="#" width="156" height="214"/></router-link>
+					<p>数据模仿</p>
+					<div class="lietu">
+						<img src="*"/>
+						<span>pink</span>
+						<router-link to='#' tag="h5">点赞</router-link>
+					</div>
+				</li>
+				<li>
+					<router-link to='/#'><img src="#" width="156" height="214"/></router-link>
+					<p>数据模仿</p>
+					<div class="lietu">
+						<img src="*"/>
+						<span>pink</span>
+						<router-link to='#' tag="h5">点赞</router-link>
+					</div>
+				</li>
+				<li>
+					<router-link to='/#'><img src="#" width="156" height="214"/></router-link>
+					<p>数据模仿</p>
+					<div class="lietu">
+						<img src="*"/>
+						<span>pink</span>
+						<router-link to='#' tag="h5">点赞</router-link>
+					</div>
+				</li>
+			</ul>			
 		</div>
 	</div>
 </template>
