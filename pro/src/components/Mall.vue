1d9ae21c37ec546e14374aa536e97ee24807f945
LXB <biaobin@qq.com>
2019-01-07 19:52:11 +0800

    xiugai20

--- pro/src/components/Mall.vue
+++ pro/src/components/Mall.vue
@@ -1,219 +1,125 @@
 <template>
-  <div class="hello">
- <header>
- 	<img :src="src">
-	<div class="mint-searchbar">
-		<router-link to="/search" class="mint-searchbar-inner" style="background: #fff;">
-			<i class="mintui mintui-search"></i>
-			<input type="search" placeholder="大家都在搜“玩转春节”" class="mint-searchbar-core" style="background: #fff;">
-		</router-link>
-	</div>
- </header>
-<!--<nav>
-	 	<router-link to="/MallRecommend" tag="span">推荐</router-link>
-		<router-link to="/MallSkin" tag='span'>护肤</router-link>
-		<router-link to="/MallCosmetics" tag='span'>彩妆</router-link>
-</nav>-->
-<mt-navbar v-model="active">
-  <mt-tab-item id="MallRecommend">推荐</mt-tab-item>
-  <mt-tab-item id="MallSkin">护肤</mt-tab-item>
-  <mt-tab-item id="MallCosmetics">彩妆</mt-tab-item>
-</mt-navbar>
+	<div class="hello">
+		<header>
+			<img :src="src">
+			<div class="mint-searchbar">
+				<router-link to="/search" class="mint-searchbar-inner" style="background: #fff;">
+					<i class="mintui mintui-search"></i>
+					<input type="search" placeholder="大家都在搜“玩转春节”" class="mint-searchbar-core" style="background: #fff;">
+				</router-link>
+			</div>
+		</header>
 
-	<section>
-		<!--<router-view @toparent="getdata"></router-view>-->
-		<mt-tab-container v-model="active" swipeable="true">
-  <mt-tab-container-item id="MallRecommend">
-  	<aside>
-			<mt-swipe :auto="3000" style="height:200px">
-			  <mt-swipe-item style="background: red;">1</mt-swipe-item>
-			  <mt-swipe-item style="background: yellow;">2</mt-swipe-item>
-			  <mt-swipe-item style="background: pink;">3</mt-swipe-item>
-			  <mt-swipe-item style="background: green;">4</mt-swipe-item>
-			</mt-swipe>
-			<router-link to="/MallActivity" class="activity">
-				<div>
-					<img src="" width="80" height="80"/>
-					<div>
-						<p>春节来了</p>
-						<p style="font-size: 16px;">抢春节特惠</p>
-					</div>
-				</div>
-				<span>GO</span>
-			</router-link>
-			<div class="preferential">
-				<router-link to="/MallSeckill">
-					<p>春节同价限时购</p>
-					<div style="margin-top: 10px;">
-						<img src="" width="50" height="50" style="margin-right: 20px;"/>
-						<img src="" width="50" height="50" />
-					</div>
-				</router-link>
-				<router-link to="/MallPreferential">
-					<p>春节预售</p>
-					<div style="margin-top: 10px;">
-						<img src="" width="50" height="50" style="margin-right: 20px;"/>
-						<img src="" width="50" height="50" />
-					</div>
-				</router-link>
-			</div>
-			<div class="preferential sale">
-				<router-link to="/MallSuperpreferential">
-					<p>超值特惠</p>
-					<div>
-						<img src="" width="60" height="60" />
-					</div>
-				</router-link>
-				<router-link to="/MallPopular">
-					<p>人气产品</p>
-					<div>
-						<img src="" width="60" height="60" />
-					</div>
-				</router-link>
-				<router-link to="/MallShop">
-					<p>小红店</p>
-					<div>
-						<img src="" width="60" height="60" />
-					</div>
-				</router-link>
-			</div>
-    <!--<mt-cell v-for="n in 15" :title="'内容 ' + n" />-->
-    <ul class="list">
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    </ul>
-		</aside>
-  </mt-tab-container-item>
-  <mt-tab-container-item id="MallSkin">
-    <!--<mt-cell v-for="n in 14" :title="'测试 ' + n" />-->
-    <aside class="aside">
-    <ul class="list">
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    </ul>
-    </aside>
-  </mt-tab-container-item>
-  <mt-tab-container-item id="MallCosmetics">
-    <!--<mt-cell v-for="n in 16" :title="'选项 ' + n" />-->
-    <aside class="aside">
-    <ul class="list">
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    	<li>
-    		<img src="" width="150" height="150">
-    		<p>滋润丰盈肌肤神器</p>
-    		<p>兰蔻精华修复肌底液</p>
-    		<p>￥200</p>
-    	</li>
-    </ul>
-    </aside>
-  </mt-tab-container-item>
-</mt-tab-container>
-   	</section>
-   	<footer>
-   		<router-link to="/" tag="span">首页</router-link>
-   		<router-link to="/mall" tag='span'>商城</router-link>
-   		<router-link to="/photograph" tag='span'>+</router-link>
-   		<router-link to="/news" tag='span'>消息</router-link>
-   		<router-link to='/myself' tag="span">我</router-link>
-   	</footer>
-  </div>
+		<mt-navbar v-model="active">
+			<mt-tab-item id="MallRecommend">推荐</mt-tab-item>
+			<mt-tab-item id="MallSkin">护肤</mt-tab-item>
+			<mt-tab-item id="MallCosmetics">彩妆</mt-tab-item>
+		</mt-navbar>
+
+		<section>
+			<mt-tab-container v-model="active" swipeable="true">
+				<mt-tab-container-item id="MallRecommend">
+					<aside>
+						<mt-swipe :auto="3000" style="height:200px" stopPropagation="true" prevent="true">
+							<mt-swipe-item style="background: red;">1</mt-swipe-item>
+							<mt-swipe-item style="background: yellow;">2</mt-swipe-item>
+							<mt-swipe-item style="background: pink;">3</mt-swipe-item>
+							<mt-swipe-item style="background: green;">4</mt-swipe-item>
+						</mt-swipe>
+						<router-link to="/MallActivity" class="activity">
+							<div>
+								<img src="" width="80" height="80" />
+								<div>
+									<p>春节来了</p>
+									<p style="font-size: 16px;">抢春节特惠</p>
+								</div>
+							</div>
+							<span>GO</span>
+						</router-link>
+						<div class="preferential">
+							<router-link to="/MallSeckill">
+								<p>春节同价限时购</p>
+								<div style="margin-top: 10px;">
+									<img src="" width="50" height="50" style="margin-right: 20px;" />
+									<img src="" width="50" height="50" />
+								</div>
+							</router-link>
+							<router-link to="/MallPreferential">
+								<p>春节预售</p>
+								<div style="margin-top: 10px;">
+									<img src="" width="50" height="50" style="margin-right: 20px;" />
+									<img src="" width="50" height="50" />
+								</div>
+							</router-link>
+						</div>
+						<div class="preferential sale">
+							<router-link to="/MallSuperpreferential">
+								<p>超值特惠</p>
+								<div>
+									<img src="" width="60" height="60" />
+								</div>
+							</router-link>
+							<router-link to="/MallPopular">
+								<p>人气产品</p>
+								<div>
+									<img src="" width="60" height="60" />
+								</div>
+							</router-link>
+							<router-link to="/MallShop">
+								<p>小红店</p>
+								<div>
+									<img src="" width="60" height="60" />
+								</div>
+							</router-link>
+						</div>
+						
+						<ul class="mallList">
+							<li>
+								<img src="" width="150" height="150">
+								<p>滋润丰盈肌肤神器</p>
+								<p>兰蔻精华修复肌底液</p>
+								<p>￥200</p>
+							</li>
+						</ul>
+					</aside>
+				</mt-tab-container-item>
+
+				<mt-tab-container-item id="MallSkin">
+					<aside class="aside">
+						<ul class="mallList">
+							<li>
+								<img src="" width="150" height="150">
+								<p>滋润丰盈肌肤神器</p>
+								<p>兰蔻精华修复肌底液</p>
+								<p>￥200</p>
+							</li>
+						</ul>
+					</aside>
+				</mt-tab-container-item>
+
+				<mt-tab-container-item id="MallCosmetics">
+					<aside class="aside">
+						<ul class="mallList">
+							<li>
+								<img src="" width="150" height="150">
+								<p>滋润丰盈肌肤神器</p>
+								<p>兰蔻精华修复肌底液</p>
+								<p>￥200</p>
+							</li>
+						</ul>
+					</aside>
+				</mt-tab-container-item>
+			</mt-tab-container>
+		</section>
+
+		<footer>
+			<router-link to="/" tag="span">首页</router-link>
+			<router-link to="/mall" tag='span'>商城</router-link>
+			<router-link to="/photograph" tag='span'>+</router-link>
+			<router-link to="/news" tag='span'>消息</router-link>
+			<router-link to='/myself' tag="span">我</router-link>
+		</footer>
+	</div>
 </template>
 
 <script>
@@ -246,26 +152,6 @@ header .mint-searchbar{
 .mint-tab-item-label{
 	font-size: 16px;
 	font-weight: 700;
-}
-nav{
-	height: 36px;
-	display: flex;
-	justify-content: space-around;
-	font-size: 16px;
-	line-height: 36px;
-	background: #fdfdfd;
-	color: #8c8c8c;
-}
-nav span{
-	display: block;
-	width: 35px;
-	font-weight: 900;
-}
-nav:hover span:first-of-type{
-	color: #8c8c8c;
-}
-nav:hover span:hover,nav span:first-of-type{
-	color: black;
 }
 aside{
 	padding: 0 15px;
@@ -312,8 +198,9 @@ aside .preferential{
 	justify-content: space-between;
 	width: 100%;
 }
-aside .preferential p{
+aside p{
 	height: 20px;
+	line-height: 20px;
 }
 aside .preferential a{
 	display: flex;
@@ -336,25 +223,24 @@ aside .preferential.sale a{
 aside .preferential.sale p{
 	margin-left: -10px;
 }
-aside .popular{
-	
-}
-ul.list{
+ul.mallList{
 	width: 100%;
 	display: flex;
 	justify-content: space-between;
 	flex-wrap: wrap;
 	font-size: 14px;
 }
-ul.list li{
+ul.mallList li{
 	padding: 10px;
 	background: #f8f8f8;
 	margin-top: 10px;
+	border-radius: 5px;
 }
-ul.list li img{
+ul.mallList li img{
 	margin-bottom: 10px;
 }
-ul.list li p:nth-of-type(2){
+ul.mallList li p:nth-of-type(2){
 	color: #8C8C8C;
+	margin-bottom: 10px;
 }
 </style>
