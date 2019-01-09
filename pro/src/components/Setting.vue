1ea2523d4d36ed74e165f4bc24757847ed6c0cf9
zhangyue <1606886383@qq.com>
2019-01-09 17:01:50 +0800

    change 19

---
+++ pro/src/components/Setting.vue
@@ -0,0 +1,21 @@
+<template>
+	<!--设置-->
+	<div class="hello">
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Setting',
+		data(){
+			return{
+				src:''
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
