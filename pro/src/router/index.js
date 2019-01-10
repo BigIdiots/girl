f5ab61c86cd62ca61e7a790c6de735cebca82760
zhangyue <1606886383@qq.com>
2019-01-10 19:34:46 +0800

    change 26

--- pro/src/router/index.js
+++ pro/src/router/index.js
@@ -30,8 +30,6 @@ import ShareDetail from '@/components/ShareDetail'//分享详情
 import Official from '@/components/Official'//客服对话
 import Setting from '@/components/Setting'//设置  未写完
 import Mine from '@/components/Mine'//我的资料
-
-import Setting from '@/components/Setting'//设置
 Vue.use(Router)
 
 export default new Router({
