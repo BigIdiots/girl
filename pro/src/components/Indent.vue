4ede85610fe66cd97995ec96d3d5c4fcde82fd4c
zhangyue <1606886383@qq.com>
2019-01-12 21:17:41 +0800

    change 33

--- pro/src/components/Indent.vue
+++ pro/src/components/Indent.vue
@@ -1,19 +1,38 @@
 <template>
-	
+	<div class="hello">
+		<mt-header title="我的订单" style="background: #F8F8F8;color: #101010;">
+			  <router-link to="#" slot="left">
+			    <mt-button icon="back" @click="back">返回</mt-button>
+			  </router-link>
+			</mt-header>
+		
+		<mt-navbar v-model="active">
+			<mt-tab-item id="1">全部</mt-tab-item>
+			<mt-tab-item id="2">待付款</mt-tab-item>
+			<mt-tab-item id="3">待收货</mt-tab-item>
+			<mt-tab-item id="4">已完成</mt-tab-item>
+			<mt-tab-item id="5">已取消</mt-tab-item>
+		</mt-navbar>
+		
+		
+		
+	</div>
 	
 </template>
 
 <script>
 	export default {
-  name: 'Indent',
-  data () {
-    return {
-      src: ''
-    }
-  }
-}
+		name:'Indent',
+     data() {
+     return {
+       		src:''
+     	}
+    
+     },
+    
+     }
 </script>
 
-<style>
+<style >
 
 </style>
